d3c90b060e932cecc1f8385d95ab00faff5e5361
Psyduck.Mans <t-bird.mans@hotmail.com>
2013-09-23 22:56:12 +0800

    1.0.0master: Add class Demo\PrintUtil
    Change-Id: Ic4e3c57086d41bea09a0a265a138533f183958be

---
+++ src/Demo/PrintUtil.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Demo
+ * @author Psyduck.Mans
+ * @link https://www.not-bad.org
+ * @license GPLv3
+ */
+
+namespace Demo;
+
+class PrintUtil {
+	/**
+	 * print line by input string
+	 * @param string $string
+	 */
+	public function printLine($string) {
+		print $strings."\r\n";
+	}
+}
