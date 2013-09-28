ecd316ace85f659ccc936e34e2161e4583c6021c
Psyduck.Mans <t-bird.mans@hotmail.com>
2013-09-28 22:21:15 +0800

    1.0.0master: update files

--- src/Demo/PrintUtil.php
+++ src/Demo/PrintUtil.php
@@ -1,19 +1,11 @@
 <?php
-/**
- * @package Demo
- * @author Psyduck.Mans
- * @link https://www.not-bad.org
- * @license GPLv3
- */
-
 namespace Demo;
 
+/**
+ * @author psyduck.mans
+ */
 class PrintUtil {
-	/**
-	 * print line by input string
-	 * @param string $string
-	 */
-	public function printLine($string) {
-		print $strings."\r\n";
+	public function printLine($str) {
+		print $str."\r\n";
 	}
 }
